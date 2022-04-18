4dc85f2932c7755c9877809e963cf5af0d4814f7
a232152000 <a232152000@gmail.com>
2022-04-18 22:43:56 +0800

    [feature]加入line與stock相關migrate tables

---
+++ database/migrations/2022_04_16_033827_create_stocks_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('stocks', function (Blueprint $table) {
+            $table->id();
+            $table->string('code',20)->comment('股票代號');
+            $table->string('ex',6)->comment('tse(上市) or otc(上櫃)');
+            $table->string('n',45)->comment('公司簡稱');
+            $table->string('nf',100)->comment('公司全名');
+            $table->float('z', 7, 3)->comment('當盤成交價');
+            $table->float('o', 7, 3)->comment('開盤');
+            $table->float('h', 7, 3)->comment('最高');
+            $table->float('l', 7, 3)->comment('最低');
+            $table->float('y', 7, 3)->comment('昨收');
+            $table->timestamps();
+
+//            $table->primary('id');
+            $table->unique('code');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('stocks');
+    }
+};
---
+++ database/migrations/2022_04_16_041036_create_line_user_stock_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('line_user_stock', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedInteger('line_user_id');
+            $table->unsignedInteger('stock_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('line_user_stock');
+    }
+};
